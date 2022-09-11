c549ed8f8e6e557502f40ff2f439c63f851ddceb
Ubong <ubongoben@gmail.com>
2022-09-10 18:05:20 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l = 'a';
+
+	for (l = 'a'; l <= 'z'; l++)
+	putchar(l);
+	putchar('\n');
+	/* your code goes there */
+	return (0);
+}
