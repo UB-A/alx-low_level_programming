bf49f9b7abe152006a67a64b76aa85fa29f6db4f
Ubong <ubongoben@gmail.com>
2022-09-11 00:43:48 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Etry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d = 48;
+
+	while (d < 58)
+	{
+		putchar(d);
+	if (d != 57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	return d++;
+	}
+	putchar('\n');
+	return (0);
+}
