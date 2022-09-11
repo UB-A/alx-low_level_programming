ecd59821c0587b7007bfea73478da4a665fc7c7e
Ubong <ubongoben@gmail.com>
2022-09-10 18:20:00 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int l = 0;
+
+	for (l = 0; l < 10; l++)
+	putchar(l);
+	putchar('\n');
+	/* your code goes there */
+	return (0);
+}
+
