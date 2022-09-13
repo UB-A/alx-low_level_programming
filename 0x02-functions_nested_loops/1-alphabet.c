c28e430033b26b9ab5b16e6748b109e1e4f7c36d
Ubong <ubongoben@gmail.com>
2022-09-13 06:40:48 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	void print_alphabet(void);
+
+		char n;
+
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			putchar(n);
+		}
+		putchar('\n');
+}
