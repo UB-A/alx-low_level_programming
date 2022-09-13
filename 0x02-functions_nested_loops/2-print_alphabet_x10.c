afa9c08192953a2cdfedafe60e899e9c542fe393
Ubong <ubongoben@gmail.com>
2022-09-13 11:10:18 -0700

    fixes

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - a function that prints 10 times the alphabet
+*
+* Description: 'prining 10 times'
+*
+*Return: Always void
+*/
+
+void print_alpabet_x10(void)
+{
+	int n, co;
+
+	co = 1;
+
+	while (co <= 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		co++;
+		_putchar('\n');
+	}
+}
