efe8bdfa2a8f78a9477e54f7d4f2da1bdb91f15b
Ubong <ubongoben@gmail.com>
2022-09-30 08:51:21 -0700

    argc task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function to print out number of arguments
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
