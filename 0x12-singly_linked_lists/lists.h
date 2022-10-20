d0dd4f92b0815d13c05851826b275a7f1a0c5ea9
Ubong <ubongoben@gmail.com>
2022-10-20 08:24:38 -0700

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
-#include <stdio.h>
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -12,7 +12,6 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
-
 typedef struct list_s
 {
 	char *str;
@@ -27,4 +26,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* LISTS_H_ */
+#endif /* _LISTS_H_ */
